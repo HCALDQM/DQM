6d117a88088bce4cf59c46d34e0785710f9ba2a9
vkhristenko <vdkhristenko1991@gmail.com>
2015-12-02 10:34:21 +0100

    adding OfflineSequence

---
+++ HcalTasks/python/OfflineSequence.py
@@ -0,0 +1,15 @@
+#-----------------
+#	HCAL DQM Offline Sequence Definition
+#	To be used for Offline DQM importing
+#-----------------
+
+#	import the tasks
+process.load('DQM.HcalTasks.DigiTask')
+process.load('DQM.HcalTasks.RawTask')
+process.load('DQM.HcalTasks.TPTask')
+process.load('DQM.HcalTasks.RecHitTask')
+
+#	redefine some variables
+
+
+
